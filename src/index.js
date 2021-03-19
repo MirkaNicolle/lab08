f332084bd2f31eefa5a3cd6ffbceba3cc068f14f
Mirka Monzón <35476538+MirkaNicolle@users.noreply.github.com>
2021-03-19 16:47:35 -0600

    adding git ignore

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //React y ReactDOM se necesitan importar en todos los componenetes que sean parte de
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './componentes/helloWorld';
+import HelloWorld from '../componentes/helloWorld';
 
 const StartComponent = () =>{
     return <h1> New react app </h1>
