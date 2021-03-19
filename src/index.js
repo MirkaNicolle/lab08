c899ae147e4e695378b30d0c3bb1738fea563963
Mirka Monzón <35476538+MirkaNicolle@users.noreply.github.com>
2021-03-19 11:12:13 -0600

    configuracion y hello world

---
+++ src/index.js
@@ -0,0 +1,10 @@
+//React y ReactDOM se necesitan importar en todos los componenetes que sean parte de
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './componentes/helloWorld';
+
+const StartComponent = () =>{
+    return <h1> New react app </h1>
+}
+
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
