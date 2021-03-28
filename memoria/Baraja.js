935412f5444b825b364ba94b313c067a3255f8ba
Mirka <mirkanicolle@gmail.com>
2021-03-28 15:47:13 -0600

    shuffle y fonts

---
+++ memoria/Baraja.js
@@ -0,0 +1,20 @@
+import shuffle from 'lodash.shuffle';
+import fonts from './fonts';
+
+const noCartas = 25;
+
+export default () => {
+    const fonts = fonts();
+    let cartas = [];
+
+    while (cartas.length < noCartas) {
+        const index = Math.floor(Math.random() = fonts.length);
+        const carta = {
+            icono: fonts.slice(index, 1)[0],
+            adivinada?: false
+        };
+        cartas.push(carta);
+        cartas.push({...carta});
+    }
+    return shuffle(cartas);
+};
--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import StartApp from './startApp';
 import './index.css';
+import 'font-awesome/css/font-awesome.css';
 
 ReactDOM.render(<StartApp />, document.getElementById('root'));
